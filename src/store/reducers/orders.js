22f5da4bb8bf19e6030e6aff87e369b1163b9ad5
Андрей Гордиенко <andrejgordienko@Andrejs-MacBook-Air.local>
2021-04-21 06:09:13 +0300

    + deepClone func

--- src/store/reducers/orders.js
+++ src/store/reducers/orders.js
@@ -1,5 +1,20 @@
 import { DELETE_ORDER, GET_ORDERS, DOUBLE_ORDER } from '../actions/orders';
 
+const deepClone = obj => {
+    if (obj === null) return null;
+    let clone = Object.assign({}, obj);
+    Object.keys(clone).forEach(
+      key =>
+        (clone[key] =
+          typeof obj[key] === "object" ? deepClone(obj[key]) : obj[key])
+    );
+    return Array.isArray(obj) && obj.length
+      ? (clone.length = obj.length) && Array.from(clone)
+      : Array.isArray(obj)
+      ? Array.from(obj)
+      : clone;
+};
+
 const initialState = {
     orders: [],
 }
@@ -7,26 +22,11 @@ const initialState = {
 export default (state = initialState, action) => {
     switch (action.type) {
         case GET_ORDERS:
-            return {...state, orders: [...action.orders]}
+            return {...state, orders: deepClone(action.orders)}
         case DELETE_ORDER:
             const filtrdedOrders = state.orders.filter(order => order.id !== action.orderId)
             return {...state, orders: [...filtrdedOrders]}
         case DOUBLE_ORDER:
-            const deepClone = obj => {
-                if (obj === null) return null;
-                let clone = Object.assign({}, obj);
-                Object.keys(clone).forEach(
-                  key =>
-                    (clone[key] =
-                      typeof obj[key] === "object" ? deepClone(obj[key]) : obj[key])
-                );
-                return Array.isArray(obj) && obj.length
-                  ? (clone.length = obj.length) && Array.from(clone)
-                  : Array.isArray(obj)
-                  ? Array.from(obj)
-                  : clone;
-              };
-              
             const selectedOrder = state.orders.find(order => order.id === action.orderId)
             
             const newOrder = deepClone(selectedOrder);
